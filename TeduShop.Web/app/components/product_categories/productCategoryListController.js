c5cfb6e149661eb7838c3a6a65c4f75516cad7da
LT\Admin <nguyenvanty.it@gmail.com>
2018-09-05 23:04:06 +0700

    Paging for list

--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -4,18 +4,29 @@
     productCategoryListController.$inject = ['$scope', 'apiService'];
 
     function productCategoryListController($scope, apiService) {
-        $scope.productCategories = [];
+        $scope.productCategories = [];  
+        $scope.page = 0;
+        $scope.pagesCount = 0;
+        $scope.getProductCategories = getProductCategories;
 
-        $scope.getProductCagories = getProductCagories;
-
-        function getProductCagories() {
-            apiService.get('/api/productcategory/getall', null, function (result) {
-                $scope.productCategories = result.data;
+        function getProductCategories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize: 2
+                }
+            }
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
             }, function () {
                 console.log('Load productcategory failed.');
             });
         }
 
-        $scope.getProductCagories();
+        $scope.getProductCategories();
     }
 })(angular.module('tedushop.product_categories'));
