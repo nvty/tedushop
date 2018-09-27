4b8ad160de0abcb88a63c707e93efbe076cf6916
LT\Admin <nguyenvanty.it@gmail.com>
2018-09-27 14:04:01 +0700

    Add new form
    add new form

--- TeduShop.Web/Models/ProductCategoryViewModel.cs
+++ TeduShop.Web/Models/ProductCategoryViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,9 @@ namespace TeduShop.Web.Models
     {
         public int ID { set; get; }
 
+        [Required]
         public string Name { set; get; }
-
+        [Required]
         public string Alias { set; get; }
 
         public string Description { set; get; }
@@ -37,7 +39,7 @@ namespace TeduShop.Web.Models
         public string MetaKeyword { set; get; }
 
         public string MetaDescription { set; get; }
-
+        [Required]
         public bool Status { set; get; }
     }
 }
--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -11,8 +11,7 @@
         $scope.keyword = '';
 
 
-        $scope.search = search;
-
+         $scope.search = search;
         function search() {
             getProductCategories();
         }
@@ -29,9 +28,7 @@
                 if (result.data.TotalCount == 0) {
                     notificationService.displayWarning('Không có bản ghi nào được tìm thấy.')
                 }
-                else {
-                    notificationService.displaySuccess('Đã tìm thấy ' + result.data.TotalCount+  ' bản ghi.')
-                }
+                
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
