e2a2a5c7841bd31d3a128f7efb6f72f84cd5d4d8
LT\Admin <nguyenvanty.it@gmail.com>
2018-09-28 20:51:08 +0700

    update form and auto make seo title service
    update form and auto make seo title service

--- TeduShop.Web/app/shared/services/apiService.js
+++ TeduShop.Web/app/shared/services/apiService.js
@@ -8,7 +8,8 @@
     function apiService($http, notificationService) {
         return {
             get: get,
-            post: post
+            post: post,
+            put: put
         }
 
         function post(url, data, success, failure) {
@@ -23,6 +24,21 @@
                     failure(error);
                 }
                
+            });
+        }
+
+        function put(url, data, success, failure) {
+            $http.put(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status)
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required.');
+                }
+                else if (failure != null) {
+                    failure(error);
+                }
+
             });
         }
 
