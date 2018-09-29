4d68ddc796224202846978c82a165f895fbcb751
LT\Admin <nguyenvanty.it@gmail.com>
2018-09-29 17:37:19 +0700

    required form
    required form

--- TeduShop.Web/Models/ProductCategoryViewModel.cs
+++ TeduShop.Web/Models/ProductCategoryViewModel.cs
@@ -10,9 +10,9 @@ namespace TeduShop.Web.Models
     {
         public int ID { set; get; }
 
-        [Required]
+        [Required(ErrorMessage ="Yêu cầu nhập tên danh mục")]
         public string Name { set; get; }
-        [Required]
+        [Required(ErrorMessage ="Yêu cầu nhập tie6i đề SEO")]
         public string Alias { set; get; }
 
         public string Description { set; get; }
@@ -39,7 +39,7 @@ namespace TeduShop.Web.Models
         public string MetaKeyword { set; get; }
 
         public string MetaDescription { set; get; }
-        [Required]
+        [Required(ErrorMessage ="Yêu cầu nhập trạng thái")]
         public bool Status { set; get; }
     }
 }
